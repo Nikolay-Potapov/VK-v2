ec4e00254c6517b3c47c318d698d25bda996a965
Nikolay Potapov <potapov4444gmail.ru>
2021-11-26 01:13:33 +0700

    first commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Post {
+    private String IdPost;
+    private String IdAuthor;
+    private String imageURL;
+    private String text;
+    private String comments;
+    private String repost;
+    private int data;
+    private int Like;
+    private int viewsInfo;
+    private boolean bookmark;
+
+}
